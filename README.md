25e3b8c67a9456a59d435de161f0d3de919ac1c7
dipankarpaul2k <dipankarpaul2k@gmail.com>
2023-08-24 21:18:05 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# Chuck Norris Jokes Web App
+
+This is a simple web application that allows users to get Chuck Norris jokes based on different categories or select a random joke. The app fetches jokes from the Chuck Norris API and displays them on the webpage. Users can also see a list of previously fetched jokes.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Screenshots](#screenshots)
+- [Features](#features)
+- [Technologies Used](#technologies-used)
+- [Setup](#setup)
+- [Usage](#usage)
+- [Contributing](#contributing)
+
+## Introduction
+
+Chuck Norris Jokes Web App is a fun project that demonstrates how to interact with an external API to fetch and display Chuck Norris jokes. The app provides users with the ability to select a joke category or choose a random joke. It also keeps track of the fetched jokes, which are displayed in a list.
+
+## Screenshots
+
+
+
+Screenshot showing the dropdown menu.
+
+
+
+Screenshot displaying "Click Me!" button and a Chuck Norris joke retrieved from the API.
+
+## Features
+
+- Fetch jokes from the Chuck Norris API based on selected categories or random jokes.
+- Display the fetched joke on the webpage.
+- Maintain a list of previously fetched jokes.
+- Responsive design for a seamless user experience on different devices.
+
+## Technologies Used
+
+- HTML: The structure of the webpage.
+- CSS: Styling and layout of the webpage.
+- JavaScript: Interacting with the Chuck Norris API and handling user interactions.
+- Chuck Norris API: The source of jokes.
+- Git: Version control and collaboration.
+- GitHub: Hosting the repository and web app.
+
+## Setup
+
+To run this project locally, follow these steps:
+
+1. Clone the repository: `git clone <repository_url>`
+2. Navigate to the project directory: `cd chuck-norris-jokes-app`
+3. Open the `index.html` file in your preferred web browser.
+
+## Usage
+
+1. Upon opening the app, you'll see the title "Chuck Norris Jokes" along with a dropdown menu to select joke categories and a "Click Me!" button.
+2. Use the dropdown to select a category or choose "Random" for a random joke.
+3. Click the "Click Me!" button to fetch and display a Chuck Norris joke based on the selected category or randomness.
+4. The fetched joke will be displayed on the webpage.
+5. The app also maintains a list of fetched jokes below the main joke display area.
+6. To view the app's behavior, you can open the browser's developer console to see log messages.
+
+## Contributing
+
+Contributions to this project are welcome! If you find any issues or want to enhance the app, feel free to open an issue or create a pull request. Make sure to follow the existing code style and guidelines.
+
---
+++ chuckNorrisJokes.js
@@ -0,0 +1,119 @@
+// Reference Variable
+const categorySelect = document.getElementById("category");
+const getJokeBtn = document.querySelector("#getJokeBtn");
+const jokeText = document.querySelector("#jokeText");
+
+console.log("Start");
+
+// Function to fetch categories
+async function fetchJokeCategories() {
+  try {
+    const response = await fetch("https://api.chucknorris.io/jokes/categories");
+
+    // Check if response is 'OK' or not!
+    if (!response.ok) {
+      throw new Error("Network response was not ok.");
+    }
+    const categories = await response.json();
+
+    // Add "Random" option to the dropdown
+    const randomOption = document.createElement("option");
+    randomOption.value = "random";
+    randomOption.text = "Random";
+    categorySelect.appendChild(randomOption);
+
+    // Adding category options to the dropdown
+    categories.forEach((category) => {
+      const option = document.createElement("option");
+      option.value = category;
+      option.text = category;
+      categorySelect.appendChild(option);
+    });
+  } catch (error) {
+    // Printing the error in the browser console
+    console.log(`Error fetching Chuck Norris Joke Categories: ${error}`);
+    alert(
+      "Error fetching Chuck Norris Joke Categories. Please try again later."
+    );
+  }
+}
+
+// Fetching random jokes
+async function fetchRandomJoke() {
+  try {
+    const response = await fetch("https://api.chucknorris.io/jokes/random");
+
+    if (!response.ok) {
+      throw new Error("Error.");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(`Error fetching joke: ${error}`);
+    return;
+  }
+}
+
+// Fetching jokes based on category
+async function fetchCategoryJoke(category) {
+  try {
+    const response = await fetch(
+      `https://api.chucknorris.io/jokes/random?category=${category}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Error.");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(`Error fetching joke: ${error}`);
+    return;
+  }
+}
+
+// Function to get jokes from API
+async function getJokes() {
+  const selectedCategory = categorySelect.value;
+  console.log(selectedCategory);
+
+  try {
+    let jokeData;
+    if (selectedCategory === "false") {
+      alert("Please select a category or choose 'Random'.");
+      categorySelect.classList.add("error");
+      setTimeout(() => {
+        categorySelect.classList.remove("error");
+      }, 500);
+    } else if (selectedCategory === "random") {
+      jokeData = await fetchRandomJoke();
+    } else {
+      jokeData = await fetchCategoryJoke(selectedCategory);
+    }
+    displayJoke(jokeData);
+  } catch (error) {
+    console.error("Error fetching joke:", error, response.status);
+    alert("An error occurred while fetching the joke. Please try again later.");
+  }
+}
+
+// Function to display jokes
+async function displayJoke(jokeData) {
+  if (jokeData) {
+    jokeText.textContent = jokeData.value;
+
+    const li = document.createElement("li");
+    li.innerHTML = jokeData.value;
+    li.classList.add("listedJokes");
+    document.getElementById("jokeList").appendChild(li);
+  } else {
+    jokeText.textContent =
+      "Error fetching Chuck Norris joke! Please try again later.";
+  }
+}
+
+// Event listener for button 'click'
+getJokeBtn.addEventListener("click", getJokes);
+window.addEventListener("load", fetchJokeCategories);
+
+console.log("End");
